a7c017decbb71b5af65f157c1fba6b7604a8c11d
jyoshitanair <jyoshitanair123@gmail.com>
2026-02-21 00:32:08 -0800

    added button images

--- ai-takeover/scripts/word_push.gd
+++ ai-takeover/scripts/word_push.gd
@@ -5,10 +5,15 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("word_pusher")
+	visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_focus_next"):
+		visible = true
 		print("button pressed")
+		animated_sprite_2d.frame = 0 
 		animated_sprite_2d.play("hand_grab")
+		await animated_sprite_2d.animation_finished
+		visible = false
