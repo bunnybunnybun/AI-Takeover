608dcb326315b6e7dfdb3839185083ca7f5df211
Carlisle Poe <carlislepoe@gmail.com>
2026-02-21 03:18:17 -0800

    Worked on pause menu.

--- ai-takeover/scripts/pause_menu.gd
+++ ai-takeover/scripts/pause_menu.gd
@@ -5,8 +5,14 @@ func _ready():
 	pause_menu.visible = false
 	
 func _input(event):
-	if Input.is_action_pressed("ui_accept"):
-		if pause_menu.visible == false:
-			pause_menu.visible = true
-		else:
-			pause_menu.visible = false
+	if pause_menu.visible == false:
+		if Input.is_action_pressed("ui_accept"):
+			pause()
+
+func pause():
+	pause_menu.visible = true
+	get_tree().paused = true
+	
+#func resume():
+	#pause_menu.visible = false
+	#get_tree().paused = false
