590f5f14d4caf9b3138aa7d0dc9408be684659a5
jyoshitanair <jyoshitanair123@gmail.com>
2026-02-21 10:02:35 -0800

    basic intro

---
+++ ai-takeover/scenes/intro.gd
@@ -0,0 +1,61 @@
+extends Control
+@onready var fade: ColorRect = $Control/fade
+@onready var label: RichTextLabel = $Control/label
+@onready var timer: Timer = $Timer
+@onready var cursor: Label = $Control/next/cursor
+var faded = false
+var tween
+var count = 0
+var text_array = [" Your Prompt is Simple. \n Get Justice"]
+var just_pressed = false
+var elapsed_time = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	label.visible_ratio = 0.0
+	fade.modulate.a = 1.0
+	await fade_into(0.0)
+	faded = true
+	_load_text(label)
+func _next_button() -> void: 
+	label.text = text_array[0]
+	_load_text(label)
+func _load_text(text) -> void:
+	tween = create_tween()
+	var duration = 0.5+(0.06* text.text.length())
+	tween.tween_property(text,"visible_ratio",1.0,duration)
+	await tween.finished
+func _unhandled_input(event: InputEvent) -> void:
+	#if event.is_action_pressed("ui_accept"):
+		#label.visible_ratio = 0.0
+		#_next_button()
+	if event.is_action_pressed("ui_focus_next") and faded == true:
+		if not just_pressed:
+			if count == 0:
+				tween.kill()
+				label.visible_ratio = 1.0
+				count += 1
+				just_pressed = true
+			elif count == 1:
+				label.visible_ratio = 0.0
+				_next_button()
+				count = 0 
+				just_pressed = true
+			timer.start()
+		print(count)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#making the cursor flicker all the time
+	elapsed_time += delta
+	if elapsed_time >= 0.6:
+		cursor.visible = !cursor.visible
+		elapsed_time = 0.0
+func fade_into (alpha:float) -> void:
+	var tween_fade := create_tween()
+	tween_fade.tween_property(fade, "modulate:a",alpha, 3.0)
+	await tween_fade.finished
+
+
+
+func _on_timer_timeout() -> void:
+	just_pressed = false
