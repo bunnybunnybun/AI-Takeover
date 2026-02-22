a2f175f5456a59922160aec5836c45f66530de4c
jyoshitanair <jyoshitanair123@gmail.com>
2026-02-22 01:52:21 -0800

    fixing the whol porblem thingy okay guys this is important

--- ai-takeover/scripts/play.gd
+++ ai-takeover/scripts/play.gd
@@ -1,13 +1,13 @@
 extends TextureButton
-#func _process(float):
-	##print("is even in process?")
-	#if Input.is_action_just_pressed("ui_focus_next"):
-		#print("focus printed")
-#func _on_pressed():
-	#print("pressed")
-	#get_tree().change_scene_to_file("res://scenes/main.tscn")
-func _input(event: InputEvent) -> void:
-	print("press detected")
-	if event.is_action_pressed("ui_focus_next"):
-		get_tree().change_scene_to_file("res://scenes/main.tscn")
+func _process(float):
+	#print("is even in process?")
+	if Input.is_action_just_pressed("ui_focus_next"):
+		print("focus printed")
+func _on_pressed():
+	print("pressed")
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+#func _input(event: InputEvent) -> void:
+	#print("press detected")
+	#if event.is_action_pressed("ui_focus_next"):
+		#get_tree().change_scene_to_file("res://scenes/main.tscn")
 	
